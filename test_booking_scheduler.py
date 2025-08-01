519be11c66c9d1606a054f6d3d32d66f3fd6184e
sungq-lee <sungq2@gmail.com>
2025-08-01 14:30:30 +0900

    Step3-1

--- test_booking_scheduler.py
+++ test_booking_scheduler.py
@@ -143,9 +143,14 @@ def test_이메일이_있는_경우에는_이메일_발송(booking_scheduler_wit
     mail_mock.send_mail.assert_called_once()
 
 
-def test_현재날짜가_일요일인_경우_예약불가_예외처리(customer):
+def test_현재날짜가_일요일인_경우_예약불가_예외처리(mocker, customer):
     # arrange
-    booking_scheduler = TestableBookingScheduler(CAPACITY_PER_HOUR, "2021/03/28 17:00")
+    mocker_get_now = mocker.patch(
+        'booking_scheduler.BookingScheduler.get_now',
+        return_value = datetime.strptime("2021/03/28 17:00", "%Y/%m/%d %H:%M")
+    )
+
+    booking_scheduler = BookingScheduler(CAPACITY_PER_HOUR)
     schedule = Schedule(ON_THE_HOUR, UNDER_CAPACITY, customer)
 
     # act and assert
