742b1d6eba3c75b9abccf54e0c643ec620524da4
sungq-lee <sungq2@gmail.com>
2025-08-01 14:26:12 +0900

    Step2-3

--- test_booking_scheduler.py
+++ test_booking_scheduler.py
@@ -118,20 +118,19 @@ def test_예약완료시_SMS는_무조건_발송(booking_scheduler_with_sms_mock
     booking_scheduler.add_schedule(schedule)
 
     # assert
-    assert sms_mock.send_called
+    sms_mock.send.assert_called()
 
 
-def test_이메일이_없는_경우에는_이메일_미발송(booking_scheduler, customer):
+def test_이메일이_없는_경우에는_이메일_미발송(booking_scheduler_with_mail_mock, customer):
     # arrnage
-    mail_sender = TestableMailSender()
-    booking_scheduler.set_mail_sender(mail_sender)
+    booking_scheduler, mail_mock = booking_scheduler_with_mail_mock
     schedule = Schedule(ON_THE_HOUR, UNDER_CAPACITY, customer)
 
     # act
     booking_scheduler.add_schedule(schedule)
 
     # assert
-    assert mail_sender.send_mail_count == 0
+    mail_mock.send_mail.assert_not_called()
 
 
 def test_이메일이_있는_경우에는_이메일_발송(booking_scheduler_with_mail_mock, customer_with_mail):
@@ -143,7 +142,7 @@ def test_이메일이_있는_경우에는_이메일_발송(booking_scheduler_wit
     booking_scheduler.add_schedule(schedule)
 
     # assert
-    assert mail_mock.send_mail_count == 1
+    mail_mock.send_mail.assert_called_once()
 
 
 def test_현재날짜가_일요일인_경우_예약불가_예외처리(customer):
