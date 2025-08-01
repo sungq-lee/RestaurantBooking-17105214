9d91c8f017b07d6cab767f938d9a35beb2deb311
sungq-lee <sungq2@gmail.com>
2025-08-01 14:32:49 +0900

    Step3-3

--- test_booking_scheduler.py
+++ test_booking_scheduler.py
@@ -15,15 +15,6 @@ UNDER_CAPACITY = 1
 CAPACITY_PER_HOUR = 3
 
 
-class TestableBookingScheduler(BookingScheduler):
-    def __init__(self, capacity_per_hour, date_time: str):
-        super().__init__(capacity_per_hour)
-        self._date_time = date_time
-
-    def get_now(self):
-        return datetime.strptime(self._date_time, "%Y/%m/%d %H:%M")
-
-
 @pytest.fixture
 def customer(mocker):
     customer = mocker.Mock()
@@ -147,7 +138,7 @@ def test_현재날짜가_일요일인_경우_예약불가_예외처리(mocker, c
     # arrange
     mocker_get_now = mocker.patch(
         'booking_scheduler.BookingScheduler.get_now',
-        return_value = datetime.strptime("2021/03/28 17:00", "%Y/%m/%d %H:%M")
+        return_value=datetime.strptime("2021/03/28 17:00", "%Y/%m/%d %H:%M")
     )
 
     booking_scheduler = BookingScheduler(CAPACITY_PER_HOUR)
