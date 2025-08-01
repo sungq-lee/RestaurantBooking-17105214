2dd80c7208da308ab8ca641d0b6df4268c77c95f
sungq-lee <sungq2@gmail.com>
2025-08-01 12:59:37 +0900

    Step5

--- test_booking_scheduler.py
+++ test_booking_scheduler.py
@@ -5,7 +5,7 @@ from schedule import Customer, Schedule
 from communication import SmsSender, MailSender
 from booking_scheduler import BookingScheduler
 
-from datetime import datetime
+from datetime import datetime, timedelta
 
 NOT_ON_THE_HOUR = datetime.strptime("2021/03/26 09:05", "%Y/%m/%d %H:%M")
 ON_THE_HOUR = datetime.strptime("2021/03/26 09:00", "%Y/%m/%d %H:%M")
@@ -51,7 +51,21 @@ def test_시간대별_인원제한이_있다_같은_시간대에_Capacity_초과
         booking_scheduler.add_schedule(new_schedule)
 
 
-def test_시간대별_인원제한이_있다_같은_시간대가_다르면_Capacity_차있어도_스케쥴_추가_성공():
+def test_시간대별_인원제한이_있다_같은_시간대가_다르면_Capacity_차있어도_스케쥴_추가_성공(booking_scheduler):
+    # arrange
+    schedule = Schedule(ON_THE_HOUR, CAPACITY_PER_HOUR, CUSTOMER)
+    booking_scheduler.add_schedule(schedule)
+
+    # act
+    different_hour = ON_THE_HOUR + timedelta(hours=1)
+    new_schedule = Schedule(different_hour, UNDER_CAPACITY, CUSTOMER)
+    booking_scheduler.add_schedule((new_schedule))
+
+    # assert
+    assert booking_scheduler.has_schedule(schedule)
+    assert booking_scheduler.has_schedule((new_schedule))
+
+
     pass
 
 
