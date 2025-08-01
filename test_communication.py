b752c0f109fbb0a3a1be058eff5bc5af91d22b54
sungq-lee <sungq2@gmail.com>
2025-08-01 13:13:59 +0900

    Step8-1

--- test_communication.py
+++ test_communication.py
@@ -1,4 +1,5 @@
-from communication import SmsSender
+from communication import SmsSender, MailSender
+
 
 class TestableSmsSender(SmsSender):
     def __init__(self):
@@ -11,3 +12,15 @@ class TestableSmsSender(SmsSender):
     @property
     def send_called(self) -> bool:
         return self._send_called
+
+
+class TestableMailSender(MailSender):
+    def __init__(self):
+        self._send_mail_count = 0
+
+    def send_mail(self, schedule):
+        self._send_mail_count += 1
+
+    @property
+    def get_mail_count(self) -> int:
+        return self._send_mail_count
