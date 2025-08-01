081f9dc29ba1e8c762ba3006bfcec83a9b1a1b92
sungq-lee <sungq2@gmail.com>
2025-08-01 13:06:39 +0900

    Step6-2

--- test_booking_scheduler.py
+++ test_booking_scheduler.py
@@ -7,6 +7,8 @@ from booking_scheduler import BookingScheduler
 
 from datetime import datetime, timedelta
 
+from test_communication import TestableSmsSender
+
 NOT_ON_THE_HOUR = datetime.strptime("2021/03/26 09:05", "%Y/%m/%d %H:%M")
 ON_THE_HOUR = datetime.strptime("2021/03/26 09:00", "%Y/%m/%d %H:%M")
 CUSTOMER = Customer("Fake name", "010-1234-5678")
@@ -69,7 +71,18 @@ def test_시간대별_인원제한이_있다_같은_시간대가_다르면_Capac
     pass
 
 
-def test_예약완료시_SMS는_무조건_발송():
+def test_예약완료시_SMS는_무조건_발송(booking_scheduler):
+    # arrange
+    testable_sms_sender = TestableSmsSender()
+    booking_scheduler.set_sms_sender(testable_sms_sender)
+    schedule = Schedule(ON_THE_HOUR, UNDER_CAPACITY, CUSTOMER)
+
+    # act
+    booking_scheduler.add_schedule(schedule)
+
+    # assert
+    assert  testable_sms_sender.send_called
+
     pass
 
 
